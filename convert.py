c68639b2cba87ae022b95ef195d763ac2276bac2
Utkarsh Shrivastawa <92841647+utkarshshrivastawa@users.noreply.github.com>
2023-07-27 06:41:14 +0530

    image into sketch

---
+++ convert.py
@@ -0,0 +1,10 @@
+import cv2
+image = cv2.imread("2.jpg")
+grey_filter = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+invert = cv2.bitwise_not(grey_filter)
+blur = cv2.GaussianBlur(invert,(21,21),0)
+invertedblur = cv2.bitwise_not(blur)
+sketch_filter = cv2.divide(grey_filter, invertedblur,scale=256.0)
+# cv2.imwrite("output.png",sketch_filter)
+cv2.imshow('orignal image',sketch_filter)
+cv2.waitKey(0)
